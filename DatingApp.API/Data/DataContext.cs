a632853c5af50e9e70021cd16aaf23714072c05b
hm9312397@gmail.com <hm9312397@gmail.com>
2020-03-14 17:37:09 +0200

    14-03-2020

--- DatingApp.API/Data/DataContext.cs
+++ DatingApp.API/Data/DataContext.cs
@@ -8,5 +8,6 @@ namespace DatingApp.API.Data
         public DataContext(DbContextOptions<DataContext> options):base(options){}
         public DataContext(){}
         public DbSet<Value> Values { get; set; }
+        public DbSet<User> Users { get; set; }
     }
 }
